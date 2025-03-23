00dbd54314fd9298a2ead3ba22bc54af7170586d
Himanshu-n17 <nayak.himanshu2004@gmail.com>
2025-03-24 01:13:43 +0530

    DevLearn E-Learning Platform

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 import Home from "./Pages/Home";
 import Courses from "./Pages/Courses";
 import CourseDetails from "./Pages/CourseDetails";
 import Dashboard from "./Pages/Dashboard";
 import Profile from "./Pages/Profile";
+import "./App.css";
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/courses" element={<Courses />} />
-        <Route path="/courses/:id" element={<CourseDetails />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+      <div className="app">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/course/:id" element={<CourseDetails />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
-};
+}
 
 export default App;
